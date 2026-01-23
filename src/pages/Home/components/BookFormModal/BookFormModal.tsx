78857bf8d248fe6364c6532e2d971ac432ae58e6
Baldacine <wandersonse0@gmail.com>
2026-01-23 10:25:19 -0300

    test: add tests and remove unused dependencies and folders

--- src/pages/Home/components/BookFormModal/BookFormModal.tsx
+++ src/pages/Home/components/BookFormModal/BookFormModal.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from "react";
 import type { Book } from "@/@types/book";
